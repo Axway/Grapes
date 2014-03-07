38835088b4f2f717f7a72b981240eb9a7d8b0b4f
jdcoffre <jd@tranquille.ch>
2014-03-07 14:51:08 +0100

    Add sample project

---
+++ samples/maven_plugin_in_action/myProject/controller/src/main/java/org/axway/grapes/sample/maven/myproject/App.java
@@ -0,0 +1,13 @@
+package org.axway.grapes.sample.maven.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
