72e96b1973ac283dfdf216163281a48825e48dd8
Shradha Chhabra <shchhabra@axway.com>
2017-01-19 11:47:46 +0530

    ECDDEV-588 Include a dynamic link to Jira

---
+++ server/src/main/java/org/axway/grapes/server/webapp/links/JiraLinkGenerator.java
@@ -0,0 +1,43 @@
+package org.axway.grapes.server.webapp.links;
+
+import java.io.*;
+import java.net.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class JiraLinkGenerator {
+    public static String jiraLink = "https://techweb.axway.com/jira/secure/CreateIssueDetails!init.jspa?";
+
+    private String urlEncodeUTF8(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedOperationException(e);
+        }
+    }
+
+    public String generateLink(final String issueSummary, final String issueDescription, final String issueReporter) {
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.putAll(getFixedFields());
+        map.put("summary", issueSummary);
+        map.put("description", issueDescription);
+        map.put("reporter", issueReporter);
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(String.format("%s=%s", urlEncodeUTF8(entry.getKey()
+                    .toString()), urlEncodeUTF8(entry.getValue().toString())));
+        }
+        return sb.toString();
+    }
+    
+    public Map<String, Object> getFixedFields(){
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("pid", 13820);
+        map.put("issuetype", 3);
+        map.put("priority", 3);
+        return map;
+    }
+}
---
+++ server/src/test/java/org/axway/grapes/server/webapp/links/JiraLinkGeneratorTest.java
@@ -0,0 +1,50 @@
+package org.axway.grapes.server.webapp.links;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.jira.create.JiraLinkGenerator;
+
+public class JiraLinkGeneratorTest {
+    @Test
+    public void validEncodedDataTest() {
+        JiraLinkGenerator jiraLink=new JiraLinkGenerator();
+        String expectedString = "pid=13820&issuetype=3&priority=3&summary=Summary&description=Description&reporter=Toto";
+        String generatedLink = jiraLink.generateLink("Summary", "Description", "Toto");
+            assertEquals(expectedString,generatedLink);
+    }
+    
+    @Test
+    public void validEncodedDataTestWithSpace() {
+        JiraLinkGenerator jiraLink=new JiraLinkGenerator();
+        String expectedString = "pid=13820&issuetype=3&priority=3&summary=This+is+the+Summary&description=This+is+the+Description&reporter=Toto";
+        String generatedLink = jiraLink.generateLink("This is the Summary", "This is the Description", "Toto");
+            assertEquals(expectedString,generatedLink);
+    }
+    
+    @Test
+    public void validDefaultFields() {
+        JiraLinkGenerator jiraLink=new JiraLinkGenerator();
+        Map<String, Object> defaultValues=jiraLink.getFixedFields();
+        
+        // checking if all fields are existing
+        assertTrue(defaultValues.containsKey("pid"));
+        assertTrue(defaultValues.containsKey("issuetype"));
+        assertTrue(defaultValues.containsKey("priority"));
+    }
+    
+    @Test
+    public void validDefaultValues() {
+        JiraLinkGenerator jiraLink=new JiraLinkGenerator();
+        Map<String, Object> defaultValues=jiraLink.getFixedFields();
+
+        // checking if all default values are correct
+        assertEquals(13820, defaultValues.get("pid"));
+        assertEquals(3, defaultValues.get("issuetype"));
+        assertEquals(3, defaultValues.get("priority"));
+    }
+
+}
