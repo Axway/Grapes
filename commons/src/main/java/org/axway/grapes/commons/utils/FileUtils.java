e630b31b844545441054b53762848668ed8d6f51
Ganuci Marian <mganuci@axway.com>
2017-06-15 16:19:42 +0300

    Fixing Sonar issues - safer file content loading

--- commons/src/main/java/org/axway/grapes/commons/utils/FileUtils.java
+++ commons/src/main/java/org/axway/grapes/commons/utils/FileUtils.java
@@ -23,25 +23,19 @@ public final class FileUtils {
      * @param fileName String
      */
     public static void serialize(final File folder, final String content, final String fileName) throws IOException {
-        if(!folder.exists()){
+        if (!folder.exists()) {
             folder.mkdirs();
         }
 
         final File output = new File(folder, fileName);
-        FileWriter writer = null;
 
-        try {
-            writer = new FileWriter(output);
+        try (
+                final FileWriter writer = new FileWriter(output);
+        ) {
             writer.write(content);
             writer.flush();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new IOException("Failed to serialize the notification in folder " + folder.getPath(), e);
-        }
-        finally {
-            if(writer != null){
-                writer.close();
-            }
         }
     }
 
@@ -101,18 +95,14 @@ public final class FileUtils {
         // FileOutputStream instance for the file in question.
         // You don't actually write any data to the file through
         // the FileOutputStream.  Just instantiate it and close it.
-        FileOutputStream doneFOS = null;
 
-        try {
-            doneFOS = new FileOutputStream(touchedFile);
+        try (
+            FileOutputStream doneFOS = new FileOutputStream(touchedFile);
+        ) {
+            // Touching the file
         }
         catch (FileNotFoundException e) {
             throw new FileNotFoundException("Failed to the find file." + e);
-        }
-        finally {
-            if(doneFOS != null){
-                doneFOS.close();
-            }
         }
     }
 
