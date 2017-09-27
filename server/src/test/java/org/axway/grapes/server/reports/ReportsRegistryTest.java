4b8fb9845df7f94a7ddb697e501a57351617eaef
Antony <aasenov@axway.com>
2017-09-27 12:12:24 +0300

    ECDDEV-861 Added message for failed reports

--- server/src/test/java/org/axway/grapes/server/reports/ReportsRegistryTest.java
+++ server/src/test/java/org/axway/grapes/server/reports/ReportsRegistryTest.java
@@ -1,11 +1,9 @@
 package org.axway.grapes.server.reports;
 
 import org.junit.Test;
-
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 import static org.axway.grapes.server.reports.ReportId.LICENSES_PER_PRODUCT_RELEASE;
@@ -35,7 +33,18 @@ public class ReportsRegistryTest {
     public void testReportNoIdDuplicate() {
         initReports();
         Set<Integer> ids = new HashSet<>();
-        ReportsRegistry.allReports().forEach(report -> assertTrue(ids.add(report.getId())));
+        StringBuilder sb = new StringBuilder();
+
+        ReportsRegistry.allReports().forEach(report -> {
+            boolean foundDuplicate = !ids.add(report.getId());
+            if (foundDuplicate &&  ReportsRegistry.findById(report.getId()).isPresent()){
+                sb.append(report.getClass().getSimpleName());
+                sb.append(" and ");
+                sb.append(ReportsRegistry.findById(report.getId()).get().getClass().getSimpleName());
+                sb.append(" are with the same ID");
+            }
+            assertFalse(sb.toString(), foundDuplicate);
+        });
 
     }
 
