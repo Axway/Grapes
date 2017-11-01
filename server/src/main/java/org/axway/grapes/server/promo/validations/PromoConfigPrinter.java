33d54ab86227347bf88a562fe69f87fd5f7549b5
Ganuci Marian <mganuci@axway.com>
2017-11-01 16:26:43 +0200

    Changing the startup message.

--- server/src/main/java/org/axway/grapes/server/promo/validations/PromoConfigPrinter.java
+++ server/src/main/java/org/axway/grapes/server/promo/validations/PromoConfigPrinter.java
@@ -21,14 +21,14 @@ public class PromoConfigPrinter {
         b.append("Message Tag Configuration");
         b.append(System.lineSeparator());
         Arrays.stream(PromotionValidation.values())
-                .forEach(v -> b.append(String.format("- %s :: %s%s", v.name(), tagConfig.getTag(v.name()).toString(), System.lineSeparator())));
+                .forEach(v -> b.append(String.format("- %s :: %s_warning %s", v.name(), tagConfig.getTag(v.name()).toString().toLowerCase(), System.lineSeparator())));
 
         c.accept(b.toString());
     }
 
     private static void useErrorStatus(final List<String> errors, final Consumer<String> c) {
         StringBuilder b = new StringBuilder(System.lineSeparator());
-        b.append("Promotion validation(s) considered errors blocking the promotion process");
+        b.append("Promotion blockers");
         b.append(System.lineSeparator());
         errors.stream()
                 .map(PromotionValidation::valueOf)
@@ -36,7 +36,7 @@ public class PromoConfigPrinter {
 
         b.append("");
         b.append(System.lineSeparator());
-        b.append("Promotion validation(s) considered warnings generating messages");
+        b.append("Promotion warnings");
         b.append(System.lineSeparator());
         Arrays.stream(PromotionValidation.values())
                 .filter(entry -> !errors.contains(entry.name()))
